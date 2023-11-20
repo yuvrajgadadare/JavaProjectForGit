b70eff6964603f9f9708dfe77040a2e583873b9b
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-20 14:46:53 +0530

    Initial commit

---
+++ src/mypackage/Program.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class Program {
+
+	public void Display() {
+		System.out.println("Welcome to Java Program");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Program p =new Program();
+		p.Display();
+
+	}
+
+}
